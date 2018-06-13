dc85e03ef6228b341549dc7edf05f74f9c30b2a3
Isaak-Malers <isaak.malers@gmail.com>
2018-06-13 15:02:11 -0600

    Comments

--- dupFinder.py
+++ dupFinder.py
@@ -13,15 +13,14 @@ def arrayGenerator(length):
 	out.append(duplicate)
 	#randomize the output.
 	random.shuffle(out)
-
 	return [duplicate, out]
 
 
 #this is the actual algorithm:
-
 def dupFinder(array):
 	pointer = 0
 	while True:
+		#print array
 		if array[pointer]  == None:
 			return pointer
 
@@ -29,12 +28,30 @@ def dupFinder(array):
 		array[pointer] = None
 		pointer = temp
 
+def runSample(tests, size):
+	print ""
+	print "running 5000 randomly generated tests on larger datasets, only failures will print:"
+	for i in range(0, 5000):
+		seed = arrayGenerator(5000)
+		array = list(seed[1])
+		answer = seed[0]
+
+		caluclated = dupFinder(array)
+
+		if(caluclated != answer):
+			print "failed for:"
+			print seed
+			print "returned: " + str(caluclated)
 
 
 array1 = [1, 2, 2]
 array2 = [2, 1, 2]
 array3 = [2, 2, 1]
-
+# array[1] pointing to itself isn't a problem.
+# because in order to GET to looking at array[1], we would have to go to a location with a value that is already one
+# Just before the whole algorithm falls apart, it finds to solution.
+badCase = [3, 1, 4, 2, 2]
+veryGoodCase = [1, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2]
 
 print "running some explicit tests"
 print array1
@@ -43,17 +60,10 @@ print array2
 print dupFinder(array2)
 print array3
 print dupFinder(array3)
+print badCase
+print dupFinder(badCase)
+print veryGoodCase
+print dupFinder(veryGoodCase)
 
-print ""
-print "running 5000 randomly generated tests on larger datasets, only failures will print:"
-for i in range(0, 5000):
-	seed = arrayGenerator(5000)
-	array = list(seed[1])
-	answer = seed[0]
-
-	caluclated = dupFinder(array)
-
-	if(caluclated != answer):
-		print "failed for:"
-		print seed
-		print "returned: " + str(caluclated)
+#Uncomment to run a big sample:
+#runSample(500, 500)
